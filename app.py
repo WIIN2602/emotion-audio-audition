7f83d6e78dca2ad2b77e15331ac75d2823de0160
WIIN2602 <128226264+WIIN2602@users.noreply.github.com>
2026-06-02 17:56:00 +0700

    Refactor audio recorder import and update code

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-from st_audio_recorder import audio_recorder
+from streamlit_audio_recorder import audio_recorder
 
 st.set_page_config(page_title="Audio Audition Platform", layout="centered")
 
